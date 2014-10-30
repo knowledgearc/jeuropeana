3a22e1b36e4558b9f586dab05321a8b7e462ea8e
Hayden Young <hayden@knowledgearc.com>
2014-10-30 16:46:22 +0000

    Return search results based on current JSpace record details.

--- modules/mod_jeuropeana/helper.php
+++ modules/mod_jeuropeana/helper.php
@@ -5,22 +5,50 @@
  * @license     GNU General Public License version 2 or later; see LICENSE
  *
  * Contributors
- * Please feel free to add your name and email (optional) here if you have 
+ * Please feel free to add your name and email (optional) here if you have
  * contributed any source code changes.
  * Name                         Email
- * Hayden Young                 <hayden@knowledgearc.com> 
+ * Hayden Young                 <hayden@knowledgearc.com>
  */
 
 abstract class ModJEuropeanaHelper
 {
     public static function getItems($params)
     {
-        $url = 'http://europeana.eu/api/v2/search.json?&wskey=yg3fcahdP&query=Mona%20Lisa';
-    
-        $client = new JHttp();
-        
-        $response = $client->get($url);
-        
-        return json_decode($response->body);
+        $input = JFactory::getApplication()->input;
+
+        $component = $input->get('option');
+        $view = $input->get('view');
+        $id = $input->getInt('id');
+
+        $tags = array();
+
+        if ($component == 'com_jspace' && $view == 'record') {
+            $record = JSpaceRecord::getInstance($id);
+
+            foreach ($record->getTags()->itemTags as $tag) {
+                $tags[] = $tag->title;
+            }
+
+            $url = 'http://europeana.eu/api/v2/search.json?&wskey=yg3fcahdP&query='.$record->title;
+
+            foreach ($tags as $tag) {
+                $url.='&qf='.$tag;
+            }
+
+            if ($record->language != '*') {
+                $url.='&qf=LANGUAGE:'.$record->language;
+            } else {
+                $url.='&qf=LANGUAGE:'.JFactory::getLanguage()->getDefault();
+            }
+
+            $client = new JHttp();
+
+            $response = $client->get($url);
+
+            return json_decode($response->body);
+        } else {
+            return array();
+        }
     }
 }
--- modules/mod_jeuropeana/tmpl/default.php
+++ modules/mod_jeuropeana/tmpl/default.php
@@ -5,12 +5,12 @@
  * @license     GNU General Public License version 2 or later; see LICENSE
  *
  * Contributors
- * Please feel free to add your name and email (optional) here if you have 
+ * Please feel free to add your name and email (optional) here if you have
  * contributed any source code changes.
  * Name                         Email
- * Hayden Young                 <hayden@knowledgearc.com> 
+ * Hayden Young                 <hayden@knowledgearc.com>
  */
- 
+
 defined('_JEXEC') or die('Restricted access');
 ?>
 
